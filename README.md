f78211a484e2c4f85529cac549724ab5ae94f450
JimSeker <seker@uwyo.edu>
2015-03-26 10:42:57 -0600

    updated to api 21, added fragment.
    Also tested with android 5 device to ensure it still works.

--- README.md
+++ README.md
@@ -3,14 +3,15 @@ android text messaging Examples
 eclipse/ has the examples in eclipse project format, no longer updated.  Otherwise the examples are for android studio.
 
 
-smsDemo is a simple app to send and receive sms messages.  The code has been tested on android 4.4 and it works.
-  It does not attempt to be a default sms provider.  The sms are sent via an intent and the default will
-   have a copy of the message as well, but the default provider doesn't have to user do anything.
+smsDemo is a simple app to send and receive sms messages.  The code has been tested on android 4.4 and 5.0.1.  It works.
+  It does not attempt to be a default sms provider.  The sms are sent via an intent and the default sms provider will
+   have a copy of the message as well, but default provider doesn't require user intervention .
 
 smsDemo2 sends binary sms messages.  Untested in 4.4+
 
-mmsDemo is a hack to send mms messages for prior to android 4.4.  Messenger comes up and the user
-  is required to press the send button in order to make it work.
+mmsDemo is a hack to send mms messages for devices prior to android 4.4.  Messenger comes up and the user
+  is required to press the send button in order to make it work.  A default sms provider required to send and receive mms 4.4+
+   or just use the content provider to read the mms messages.
 
 
 These are example code for University of Wyoming, Cosc 4730 Mobile Programming course.
---
+++ smsDemo/app/src/main/java/edu/cs4730/smsdemo/MainFragment.java
@@ -0,0 +1,134 @@
+package edu.cs4730.smsdemo;
+
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.telephony.SmsManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/*
+ * A simple demo of how to send a sms (text) message.
+ * It also has a methods to check and see if the message was sent or failed.
+ *
+ * This was tested with android 4.4 and you can send a message this way.
+ * This app does not become the default sms provider.
+ * It also reads the sms message via broadcast receiver, but makes not attempt to change/delete the message.
+ *
+ * There is no attempt in this code to read/write MMS messages.
+ */
+public class MainFragment extends Fragment {
+
+    Button btnSendSMS;
+    EditText txtPhoneNo;
+    EditText txtMessage;
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View myView = inflater.inflate(R.layout.fragment_main, container, false);
+
+        btnSendSMS = (Button) myView.findViewById(R.id.btnSendSMS);
+        txtPhoneNo = (EditText) myView.findViewById(R.id.txtPhoneNo);
+        txtMessage = (EditText) myView.findViewById(R.id.txtMessage);
+
+        btnSendSMS.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String phoneNo = txtPhoneNo.getText().toString();
+                String message = txtMessage.getText().toString();
+                if (phoneNo.length() > 0 && message.length() > 0)
+                    sendSMS(phoneNo, message);
+                else
+                    Toast.makeText(getActivity(),
+                            "Please enter both phone number and message.", Toast.LENGTH_SHORT).show();
+            }
+        });
+        return myView;
+    }
+
+
+    /*   Short method with no monitoring.
+     *   //---sends an SMS message to another device---
+        private void sendSMS(String phoneNumber, String message) {
+            PendingIntent pi = PendingIntent.getActivity(getActivity(), 0,
+                new Intent(this, MainActivity.class), 0);
+            SmsManager sms = SmsManager.getDefault();
+            sms.sendTextMessage(phoneNumber, null, message, pi, null);
+        }
+     */
+    //---sends an SMS message to another device---
+    private void sendSMS(String phoneNumber, String message) {
+        String SENT = "SMS_SENT";
+        String DELIVERED = "SMS_DELIVERED";
+
+        PendingIntent sentPI = PendingIntent.getBroadcast(getActivity(), 0, new Intent(SENT), 0);
+
+        PendingIntent deliveredPI = PendingIntent.getBroadcast(getActivity(), 0, new Intent(DELIVERED), 0);
+
+        //---when the SMS has been sent---
+        getActivity().registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getActivity(), "SMS sent", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_GENERIC_FAILURE:
+                        Toast.makeText(getActivity(), "Generic failure", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NO_SERVICE:
+                        Toast.makeText(getActivity(), "No service", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_NULL_PDU:
+                        Toast.makeText(getActivity(), "Null PDU", Toast.LENGTH_SHORT).show();
+                        break;
+                    case SmsManager.RESULT_ERROR_RADIO_OFF:
+                        Toast.makeText(getActivity(), "Radio off", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(SENT));
+
+        //---when the SMS has been delivered---
+        getActivity().registerReceiver(new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context arg0, Intent arg1) {
+                switch (getResultCode()) {
+                    case Activity.RESULT_OK:
+                        Toast.makeText(getActivity(), "SMS delivered", Toast.LENGTH_SHORT).show();
+                        break;
+                    case Activity.RESULT_CANCELED:
+                        Toast.makeText(getActivity(), "SMS not delivered", Toast.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        }, new IntentFilter(DELIVERED));
+
+		/*
+         * These two lines below actually send the message via an intent.
+		 * The default provider does not show up and this is backward compatible to 2.3.3
+		 *
+		 */
+        SmsManager sms = SmsManager.getDefault();
+        sms.sendTextMessage(phoneNumber, null, message, sentPI, deliveredPI);
+    }
+
+
+}
